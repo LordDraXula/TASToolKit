6b1dce759ef9499d1c23d3eb5aac1cdd1fca174b
kierio04 <kieran.sinclairlomax@gmail.com>
2021-09-17 11:10:14 +1200

    Add WIPs and Revert Time Difference
    All WIP features with code have been added in (exact finish is not in _MKW_Text.lua as it crashes dolphin). Time Difference has been reverted to the pre-vector system and code has been cleaned up.

--- Sys/Scripts/_MKW_Text.lua
+++ Sys/Scripts/_MKW_Text.lua
@@ -1,19 +1,33 @@
 ----- GLOBAL VARIABLES -----
 package.path = GetScriptsDir() .. "MKW/MKW_core.lua"
 local core = require("MKW_core")
-package.path = GetScriptsDir() .. "MKW/Math_core.lua"
-local math_core = require("Math_core")
 
 package.path = GetScriptsDir() .. "MKW/MKW_Pointers.lua"
 local Pointers = require("MKW_Pointers")
 --Add an underscore (_) to the beginning of the filename if you want the script to auto launch once you start a game!
 
+local xRotPrev = 0
+local yRotPrev = 0
+local zRotPrev = 0
+local xRot = 0
+local yRot = 0
+local zRot = 0
+local xRotSpeed = 0
+local yRotSpeed = 0
+local zRotSpeed = 0
+local curFrame = 0
 
 function onScriptStart()
 	if GetGameID() ~= "RMCP01" and GetGameID() ~= "RMCJ01" and GetGameID() ~= "RMCE01" and GetGameID() ~= "RMCK01" then
 		SetScreenText("")
 		CancelScript()
 	end
+	
+	xRotPrev = core.calculateEuler().X
+	yRotPrev = core.calculateEuler().Y
+	zRotPrev = core.calculateEuler().Z
+	
+	curFrame = GetFrameCount()
 end
 
 function onScriptCancel()
@@ -22,30 +36,38 @@ end
 
 function onScriptUpdate()
 
+	if GetFrameCount() ~= curFrame then
+		xRot = core.calculateEuler().X
+		yRot = core.calculateEuler().Y
+		zRot = core.calculateEuler().Z
+		xRotSpeed = xRot - xRotPrev
+		yRotSpeed = yRot - yRotPrev
+		zRotSpeed = zRot - zRotPrev
+		xRotPrev = xRot
+		yRotPrev = yRot
+		zRotPrev = zRot
+	end
+
   local text = ""
-	text = text .. string.format("\nFrame: %d\n", core.getFrameOfInput())
+	text = text .. string.format("\nFrame: %d", core.getFrameOfInput())
 	text = text .. "\n\n===== Speed ====="
-	text = text .. string.format("\nX: %12.6f\nY: %12.6f\nZ: %12.6f\nXZ: %11.6f\nXYZ: %10.6f", core.getSpd().X, core.getSpd().Y, core.getSpd().Z, core.getSpd().XZ, core.getSpd().XYZ)
-	text = text .. "\n\n===== Rotation (Facing|Moving) ====="
-	text = text .. string.format("\nX: %6.2f | %6.2f \nY: %6.2f | %6.2f \nZ: %6.2f | %6.2f", core.calculateEuler().X, core.calculateDirectX(), core.calculateEuler().Y, core.calculateDirectY(), core.calculateEuler().Z, core.calculateDirectZ())
-	local playerStart, playerMoveFull, ghostStart, ghostMoveFull = core.getPos(), core.getSpd(), core.getPosGhost(), core.getSpdGhost()
-	local playerMove = {X = playerMoveFull.X, Y = playerMoveFull.Y, Z = playerMoveFull.Z}
+	text = text .. string.format("\nX: %8.4f | Y: %8.4f | Z: %8.4f \nXZ: %12.8f | XYZ: %12.8f", core.getSpd().X, core.getSpd().Y, core.getSpd().Z, core.getSpd().XZ, core.getSpd().XYZ)
+	text = text .. "\n\n===== Acceleration ====="
+	text = text .. string.format("\nAction: %s | %s \nBoost: %d | Normal: %1.4f | Wheelie: %1.4f \nDrift: %1.4f | Offroad: %1.4f", core.AccelRates().A, core.AccelRates().N, core.AccelRates().B, core.AccelRates().H, core.AccelRates().W, core.AccelRates().D, core.AccelRates().O)
 if core.isSinglePlayer() == false then
-	local ghostMove = {X = ghostMoveFull.X, Y = ghostMoveFull.Y, Z = ghostMoveFull.Z}
-	local distance = math_core.vectorMovementDifferenceXZ(playerStart, playerMove, ghostStart, ghostMove)
-	local avgSpeed = (playerMoveFull.XZ + ghostMoveFull.XZ) / 2
-	
+	local fd = core.calculateDifference(core.calculateEuler().Y).D -- facing direction difference
+	local fi = core.calculateDifference(core.calculateEuler().Y).I
+	local md = core.calculateDifference(core.calculateDirection().Y).D -- moving direction difference
+	local mi = core.calculateDifference(core.calculateDirection().Y).I
+	local fy = core.calculateDirection().FY -- finish line direction
+	local fyd = core.calculateDifference(fy).D -- finish direction difference
+	local fyi = core.calculateDifference(fy).I
 	text = text .. "\n\n===== Time Difference ====="
-	text = text .. string.format("\nDistance: %11.6f \nFrames: %11.6f \nTime: %11.6f", distance, distance/avgSpeed, (distance/avgSpeed)/60)
+	text = text .. string.format("\nFacing: %11.6f | %8.3f f (%s) \nMoving: %11.6f | %8.3f f (%s) \nFinish: %11.6f | %8.4f f (%s)", fd, fd*60, core.differenceText(fd, fi), md, md*60, core.differenceText(md, mi), fyd, fyd*60, core.differenceText(fyd, fyi))
 end
-	local finishLine = core.getFinishLine()
-	local XYZ1, XYZ2 = {X = finishLine.X1, Y = 0, Z = finishLine.Z1}, {X = finishLine.X2, Y = 0, Z = finishLine.Z2}
+	text = text .. "\n\n===== Rotation ====="
+	text = text .. string.format("\nFacing X (Pitch): %8.4f \nFacing Y (Yaw): %8.4f \nMoving Y (Yaw) %8.4f \nFacing Z (Roll): %8.4f", core.calculateEuler().X, core.calculateEuler().Y, core.calculateDirection().Y, core.calculateEuler().Z)
 	
-	local distance = math_core.distanceToLineXZ(playerStart, playerMove, XYZ1, math_core.subtractVector(XYZ2, XYZ1))
-
-	text = text .. "\n\n===== Distance to Finish line ====="
-	text = text .. string.format("\nDistance: %11.6f \nFrames: %11.6f \nTime: %11.6f", distance, distance/playerMoveFull.XZ, (distance/playerMoveFull.XZ)/60)
-
 	SetScreenText(text)
 end
 
